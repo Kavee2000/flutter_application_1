d27b338e0d80fd48a7d28b408cfdabf7714777d7
Kavee2000 <99233218+Kavee2000@users.noreply.github.com>
2023-07-13 10:27:20 -0700

    Initial commit

---
+++ lib/Pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'Pages/home_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
